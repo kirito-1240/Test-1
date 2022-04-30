cbf82c64ae7257d61685ec284ca24e1dd6ba7579
Kirito <96290899+kirito-1240@users.noreply.github.com>
2022-04-30 18:54:05 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -35,10 +35,10 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
-    print("Yay your userbot is officially working.!!!")
+    print("Your userbot is officially working.!!!")
     print(
-        f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        f"Congratulation, now type {cmdhr}alive to see message if TempestUB is live\
+        \nIf you need assistance, head to https://t.me/TempestUBSupport"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
